b963f1fd4c67e0710fdf6160f0196b670f6aa9a4
Faheem506pk <faheemiqbalm@gmail.com>
2024-11-05 18:31:42 +0500

    Update NotionTable.tsx

--- src/components/NotionTable.tsx
+++ src/components/NotionTable.tsx
@@ -57,7 +57,7 @@ const NotionTable: React.FC = () => {
     return savedOptions ? JSON.parse(savedOptions) : [""];
   });
 
-  const [newOption, setNewOption] = useState<string>("");
+  const [newOption, setNewOption] = useState(null);
   const [editingOption, setEditingOption] = useState<{
     oldValue: string;
     newValue: string;
