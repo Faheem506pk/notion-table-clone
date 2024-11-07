bb829e8ad51bcedf2be9a66c52ebe93518aa65fe
Faheem506pk <faheemiqbalm@gmail.com>
2024-11-07 17:16:48 +0500

    delete multiple rows

--- src/components/NotionTable.tsx
+++ src/components/NotionTable.tsx
@@ -78,17 +78,17 @@ const NotionTable: React.FC = () => {
     return storedColors ? JSON.parse(storedColors) : {};
   });
 
-  // const handleSelectAllRows = () => {
-  //   const newSelectedRows = new Set(selectedRows);
-  //   if (newSelectedRows.size === rows.length) {
-  //     // Deselect all
-  //     newSelectedRows.clear();
-  //   } else {
-  //     // Select all
-  //     rows.forEach((_, index) => newSelectedRows.add(index));
-  //   }
-  //   setSelectedRows(newSelectedRows);
-  // };
+  const handleSelectAllRows = () => {
+    const newSelectedRows = new Set(selectedRows);
+    if (newSelectedRows.size === rows.length) {
+      // Deselect all
+      newSelectedRows.clear();
+    } else {
+      // Select all
+      rows.forEach((_, index) => newSelectedRows.add(index));
+    }
+    setSelectedRows(newSelectedRows);
+  };
   const handleDeleteSelectedRows = () => {
     const remainingRows = rows.filter((_, index) => !selectedRows.has(index));
     setRows(remainingRows); // Update rows state
@@ -956,7 +956,7 @@ const NotionTable: React.FC = () => {
                                <MdDelete color="gray.300" style={{width:"20px" , height:"20px"}}/>
                              </Text>
                             )}
-                            {selectedRows.size > 0 && <input style={{width:"15px" , height:"15px"}} type="checkbox" />}
+                            {selectedRows.size > 0 && <input style={{width:"15px" , height:"15px"}} type="checkbox"  onChange={() => {handleSelectAllRows();}} />}
                             </div>
                           </Flex>
                         </Th>
